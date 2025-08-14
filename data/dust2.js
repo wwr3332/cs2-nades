b90ea5631d384b12be77b7a01d64e88b4710fc56
Vlad <vladv6542@gmail.com>
2025-08-14 13:39:51 +0300

    DevWiz auto-commit at 2025-08-14 13:39:50
    Интерактивная карта: Отображение всех траекторий (исправлено)

--- data/dust2.js
+++ data/dust2.js
@@ -2,34 +2,168 @@ export default {
     name: "Dust II",
     image: "assets/images/dust2_map.png",
     nades: [
+            {
+                "id": "dust2_blue_to_cross",
+                "from": "blue",
+                "to": "cross",
+                "type": "Дым",
+                "throwType": "Стандартный",
+                "side": "T",
+                "trajectory": [
+                    {
+                        "x": 74.94,
+                        "y": 47.67
+                    },
+                    {
+                        "x": 80.04,
+                        "y": 20.63
+                    },
+                    {
+                        "x": 80.69,
+                        "y": 23.56
+                    }
+                ],
+                "lineup": {
+                    "video": "assets/lineups/dust2/blue_to_cross.mp4",
+                    "images": [
+                        "assets/lineups/dust2/blue_to_cross_1.jpg"
+                    ]
+                }
+            },
+            {
+    "id": "dust2_t-spawn_to_long-start",
+    "from": "t-spawn",
+    "to": "long-start",
+    "type": "Дым",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
         {
-            "id": "d2_ct_cross_from_tspawn_example",
-            "from": "Т-спавн, у машины",
-            "to": "КТ-перетяжка на Б",
-            "type": "Дым",
-            "throwType": "Стандартный",
-            "side": "T",
-            "trajectory": [
-                { "x": 31.84, "y": 88.38 },
-                { "x": 50.88, "y": 58.5 }
-            ],
-            "lineup": { 
-                "video": "assets/lineups/dust2/d2_smoke_ct_cross.mp4", 
-                "images": ["assets/lineups/dust2/d2_smoke_ct_cross_1.jpg"] 
-            }
+            "x": 51.48,
+            "y": 86.43
         },
         {
-            "id": "d2_xbox_from_tspawn_example",
-            "from": "Т-спавн, за бочками",
-            "to": "Иксбокс (шорт)",
-            "type": "Дым",
-            "throwType": "Jump-throw",
-            "side": "CT",
-            "trajectory": [
-                { "x": 37.01, "y": 91.02 },
-                { "x": 50.49, "y": 38.96 }
-            ],
-            "lineup": { "video": "", "images": [] }
+            "x": 83.08,
+            "y": 45.39
         }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/t-spawn_to_long-start.mp4",
+        "images": [
+            "assets/lineups/dust2/t-spawn_to_long-start_1.jpg",
+            "assets/lineups/dust2/t-spawn_to_long-start_2.jpg",
+            "assets/lineups/dust2/t-spawn_to_long-start_3.jpg",
+            "assets/lineups/dust2/t-spawn_to_long-start_4.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_box_to_car",
+    "from": "box",
+    "to": "car",
+    "type": "Молотов",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 73.74,
+            "y": 53.64
+        },
+        {
+            "x": 92.85,
+            "y": 27.14
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/box_to_car.mp4",
+        "images": [
+            "assets/lineups/dust2/box_to_car_1.jpg",
+            "assets/lineups/dust2/box_to_car_2.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_t-spawn_to_xbox",
+    "from": "t-spawn",
+    "to": "xbox",
+    "type": "Дым",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 43.88,
+            "y": 86.86
+        },
+        {
+            "x": 47.58,
+            "y": 39.74
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/t-spawn_to_xbox.mp4",
+        "images": [
+            "assets/lineups/dust2/t-spawn_to_xbox_1.jpg",
+            "assets/lineups/dust2/t-spawn_to_xbox_2.jpg",
+            "assets/lineups/dust2/t-spawn_to_xbox_3.jpg",
+            "assets/lineups/dust2/t-spawn_to_xbox_4.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_t-spawn_to_mid-doors",
+    "from": "t-spawn",
+    "to": "mid doors",
+    "type": "Дым",
+    "throwType": "Стандартный",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 44.21,
+            "y": 76.98
+        },
+        {
+            "x": 45.4,
+            "y": 35.29
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/t-spawn_to_mid-doors.mp4",
+        "images": [
+            "assets/lineups/dust2/t-spawn_to_mid-doors_1.jpg",
+            "assets/lineups/dust2/t-spawn_to_mid-doors_2.jpg",
+            "assets/lineups/dust2/t-spawn_to_mid-doors_3.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_short_to_short",
+    "from": "short",
+    "to": "short",
+    "type": "Дым",
+    "throwType": "Стандартный",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 63.54,
+            "y": 33.77
+        },
+        {
+            "x": 66.79,
+            "y": 21.17
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/short_to_short.mp4",
+        "images": [
+            "assets/lineups/dust2/short_to_short_1.jpg",
+            "assets/lineups/dust2/short_to_short_2.jpg",
+            "assets/lineups/dust2/short_to_short_3.jpg",
+            "assets/lineups/dust2/short_to_short_4.jpg"
+        ]
+    }
+},
+
+
+            
     ]
 };
