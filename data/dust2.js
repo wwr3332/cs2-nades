ba49077ab9b0d35ad8e9735b35fb3e22b806e163
Vlad <vladv6542@gmail.com>
2025-08-16 23:55:19 +0300

    add more nades

--- data/dust2.js
+++ data/dust2.js
@@ -276,6 +276,347 @@ export default {
         ]
     }
 },
+{
+    "id": "dust2_out-tunnels_to_b-doors",
+    "from": "out-tunnels",
+    "to": "b-doors",
+    "type": "Дым",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 25.53,
+            "y": 60.69
+        },
+        {
+            "x": 25.75,
+            "y": 22.91
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/out-tunnels_to_b-doors.mp4",
+        "images": [
+            "assets/lineups/dust2/out-tunnels_to_b-doors_1.jpg",
+            "assets/lineups/dust2/out-tunnels_to_b-doors_2.jpg",
+            "assets/lineups/dust2/out-tunnels_to_b-doors_3.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_out-tunnels_to_window",
+    "from": "out-tunnels",
+    "to": "window",
+    "type": "Дым",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 25.53,
+            "y": 60.69
+        },
+        {
+            "x": 24.99,
+            "y": 11.94
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/out-tunnels_to_window.mp4",
+        "images": [
+            "assets/lineups/dust2/out-tunnels_to_window_1.jpg",
+            "assets/lineups/dust2/out-tunnels_to_window_2.jpg",
+            "assets/lineups/dust2/out-tunnels_to_window_3.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_stairs_to_a",
+    "from": "stairs",
+    "to": "A",
+    "type": "Молотов",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 60.93,
+            "y": 29.42
+        },
+        {
+            "x": 80.37,
+            "y": 15.53
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/stairs_to_a.mp4",
+        "images": [
+            "assets/lineups/dust2/stairs_to_a_1.jpg",
+            "assets/lineups/dust2/stairs_to_a_2.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_upp-tunnels_to_dog",
+    "from": "upp-tunnels",
+    "to": "dog",
+    "type": "Молотов",
+    "throwType": "CTRL+Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 6.86,
+            "y": 48.32
+        },
+        {
+            "x": 12.07,
+            "y": 35.07
+        },
+        {
+            "x": 7.94,
+            "y": 28.77
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/upp-tunnels_to_dog.mp4",
+        "images": [
+            "assets/lineups/dust2/upp-tunnels_to_dog_1.jpg",
+            "assets/lineups/dust2/upp-tunnels_to_dog_2.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_car_to_upp-tunnels",
+    "from": "car",
+    "to": "upp-tunnels",
+    "type": "Флеш",
+    "throwType": "Jump-throw",
+    "side": "CT",
+    "trajectory": [
+        {
+            "x": 14.89,
+            "y": 30.94
+        },
+        {
+            "x": 21.08,
+            "y": 32.25
+        },
+        {
+            "x": 10.77,
+            "y": 30.29
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/car_to_upp-tunnels.mp4",
+        "images": [
+            "assets/lineups/dust2/car_to_upp-tunnels_1.jpg",
+            "assets/lineups/dust2/car_to_upp-tunnels_2.jpg"
+        ]
+    }
+},
+
+{
+    "id": "dust2_upp-tunnels_to_b",
+    "from": "upp-tunnels",
+    "to": "B",
+    "type": "Флеш",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 17.07,
+            "y": 43.21
+        },
+        {
+            "x": 17.5,
+            "y": 31.6
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/upp-tunnels_to_b.mp4",
+        "images": [
+            "assets/lineups/dust2/upp-tunnels_to_b_1.jpg",
+            "assets/lineups/dust2/upp-tunnels_to_b_2.jpg"
+        ]
+    }
+},
+
+{
+    "id": "dust2_lover-tunnels_to_ct-mid",
+    "from": "lover-tunnels",
+    "to": "ct-mid",
+    "type": "Флеш",
+    "throwType": "CTRL+Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 43.02,
+            "y": 40.83
+        },
+        {
+            "x": 44.86,
+            "y": 30.51
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/lover-tunnels_to_ct-mid.mp4",
+        "images": [
+            "assets/lineups/dust2/lover-tunnels_to_ct-mid_1.jpg",
+            "assets/lineups/dust2/lover-tunnels_to_ct-mid_2.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_ct-mid_to_t-mid",
+    "from": "ct-mid",
+    "to": "t-mid",
+    "type": "Флеш",
+    "throwType": "Left + Right Click ",
+    "side": "CT",
+    "trajectory": [
+        {
+            "x": 48.44,
+            "y": 32.14
+        },
+        {
+            "x": 45.73,
+            "y": 36.48
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/ct-mid_to_t-mid.mp4",
+        "images": [
+            "assets/lineups/dust2/ct-mid_to_t-mid_1.jpg",
+            "assets/lineups/dust2/ct-mid_to_t-mid_2.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_a-car_to_long",
+    "from": "a-car",
+    "to": "long",
+    "type": "Флеш",
+    "throwType": "Стандартный",
+    "side": "CT",
+    "trajectory": [
+        {
+            "x": 94.92,
+            "y": 30.18
+        },
+        {
+            "x": 92.85,
+            "y": 28.01
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/a-car_to_long.mp4",
+        "images": [
+            "assets/lineups/dust2/a-car_to_long_1.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_cross_to_long",
+    "from": "cross",
+    "to": "long",
+    "type": "Флеш",
+    "throwType": "Left + Right Click",
+    "side": "CT",
+    "trajectory": [
+        {
+            "x": 74.72,
+            "y": 26.17
+        },
+        {
+            "x": 83.51,
+            "y": 25.95
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/cross_to_long.mp4",
+        "images": [
+            "assets/lineups/dust2/cross_to_long_1.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_out-long_to_long",
+    "from": "out-long",
+    "to": "long",
+    "type": "Флеш",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 62.56,
+            "y": 80.46
+        },
+        {
+            "x": 78.52,
+            "y": 61.67
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/out-long_to_long.mp4",
+        "images": [
+            "assets/lineups/dust2/out-long_to_long_1.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_box_to_long",
+    "from": "box",
+    "to": "long",
+    "type": "Флеш",
+    "throwType": "Jump-throw",
+    "side": "T",
+    "trajectory": [
+        {
+            "x": 71.79,
+            "y": 63.95
+        },
+        {
+            "x": 66.14,
+            "y": 59.5
+        },
+        {
+            "x": 70.59,
+            "y": 53.85
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/box_to_long.mp4",
+        "images": [
+            "assets/lineups/dust2/box_to_long_1.jpg"
+        ]
+    }
+},
+{
+    "id": "dust2_long_to_box",
+    "from": "long",
+    "to": "box",
+    "type": "Флеш",
+    "throwType": "Стандартный",
+    "side": "CT",
+    "trajectory": [
+        {
+            "x": 82.97,
+            "y": 43.11
+        },
+        {
+            "x": 82.97,
+            "y": 37.57
+        },
+        {
+            "x": 84.06,
+            "y": 45.39
+        }
+    ],
+    "lineup": {
+        "video": "assets/lineups/dust2/long_to_box.mp4",
+        "images": [
+            "assets/lineups/dust2/long_to_box_1.jpg"
+        ]
+    }
+},
+
 
 
 
